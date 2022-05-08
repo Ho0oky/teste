c8a2dcaf234a74d3feecb8935577b0602eff1e65
Ho0oky <vi.alves74@gmail.com>
2022-05-08 16:45:51 -0300

    Braz

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+client.on("ready", () => {
+    console.log(`O bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`);
+    client.user.setGame(`Estou em ${client.guilds.size} servidores`);
+});
+
+client.on("guildCreate", guild => {
+    console.log(`O bot entrou nos serivores: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
+    client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+
+client.on("guildDelete", guild => {
+    console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+
+});
+
+client.login(config.token);
